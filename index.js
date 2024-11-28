cc6b6e79e8c309072d077a346d9059a7aca31e2e
Matheus Assunção <78885003+Umhenrique@users.noreply.github.com>
2024-11-28 20:20:01 -0300

    V0.1
    simple particle and holding feature

---
+++ index.js
@@ -0,0 +1,104 @@
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+let mouse = { x: null, y: null };
+let isMouseHeld = false;
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.size *= 0.95;
+  }
+
+  draw() {
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+class HoldClick {
+  constructor(target, callback) {
+    this.target = target;
+    this.callback = callback;
+    this.isHeld = false;
+
+    ["mousedown", "touchstart"].forEach(type => {
+      this.target.addEventListener(type, this._onHoldStart.bind(this));
+    });
+    ["mouseup", "mouseleave", "mouseout", "touchend", "touchcancel"].forEach(type => {
+      this.target.addEventListener(type, this._onHoldEnd.bind(this));
+    });
+  }
+
+  _onHoldStart(e) {
+    isMouseHeld = true;
+    this.isHeld = true;
+    const loop = () => {
+      if (this.isHeld) {
+        this.callback();
+        requestAnimationFrame(loop);
+      }
+    };
+    loop();
+  }
+
+  _onHoldEnd() {
+    isMouseHeld = false;
+    this.isHeld = false;
+  }
+}
+
+const createParticle = () => {
+  particles.push(new Particle(mouse.x, mouse.y));
+};
+
+new HoldClick(canvas, createParticle);
+
+canvas.addEventListener('mousemove', (e) => {
+  const rect = canvas.getBoundingClientRect();
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+
+function drawText() {
+  ctx.fillStyle = '#ffffff';
+  ctx.font = '24px Arial';
+  ctx.fillText('Left click and hold', canvas.width / 2 - 100, canvas.height / 2);
+}
+
+function handleParticles() {
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].draw();
+    if (particles[i].size <= 0.5) {
+      particles.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  if (!isMouseHeld && particles.length === 0) {
+    drawText();
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Studying Particles in JavaScript
+
+This project is a simple study of particles in JavaScript, created solely for practice purposes. The goal is to understand and experiment with particle systems, animations, and interactions using JavaScript.
+
+## Features
+
+- Basic particle creation and rendering
+- Particle movement and animation
+- Interaction with mouse events
+
+## Getting Started
+
+To run the project, simply open the `index.html` file in your web browser.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## GOALS
+
+- Editing particle
+    - Size
+    - Color
+    - speed
+    - format
